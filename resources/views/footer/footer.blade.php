4acda4aeba24934f8fcad41c5c487d9c64741875
nopalpadail18 <nopalpadail@gmail.com>
2024-03-02 14:22:50 +0800

    first commit

---
+++ resources/views/footer/footer.blade.php
@@ -0,0 +1,13 @@
+{{-- Footer --}}
+<div class="container bg-danger">
+    <div class="container">
+        <div class="row text-center text-white">
+            <div class="col-lg-12">
+                <small>
+                    <p class="mt-3">&copy;TEKNIK JARINGAN KOMPUTER DAN TELEKOMUNIKASI</p>
+                </small>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- End Footer --}}
